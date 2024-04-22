5cf04242b294958d36d372c2d18b7f132eaf1264
Shikder-Tarek-15 <shikdertarek2019@gmail.com>
2024-04-22 09:18:44 +0600

    Add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    const user = { email, password };
+    console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
 
   return (
     <>
       <h1>Simple CRUD</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" id="" />
+        <input type="password" name="password" id="" />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
